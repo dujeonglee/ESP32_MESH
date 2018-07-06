b4d8ea18aeeb0c8c3389b4c2ba82c985faf1eb82
Dujeong Lee <dujeong.lee82@gmail.com>
2018-07-06 22:24:34 +0900

    Create dhcp_relay.h

---
+++ main/dhcp_relay.h
@@ -0,0 +1,16 @@
+#ifndef DJMESH_DHCP_RELAY_H
+#define DJMESH_DHCP_RELAY_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void start_dhcp_relay();
+    void stop_dhcp_relay();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
