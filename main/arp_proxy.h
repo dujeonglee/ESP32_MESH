ee2d443e8eae0435c5b71ab83076d0d29264adbe
Dujeong Lee <dujeong.lee82@gmail.com>
2018-07-06 22:23:38 +0900

    Create arp_proxy.h

---
+++ main/arp_proxy.h
@@ -0,0 +1,16 @@
+#ifndef DJMESH_ARP_PROXY_H
+#define DJMESH_ARP_PROXY_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void start_arp_proxy();
+    void stop_arp_proxy();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
