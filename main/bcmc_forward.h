c4cea7340f88d44a527ce05ee616b434e03fd4d4
Dujeong Lee <dujeong.lee82@gmail.com>
2018-07-31 23:45:32 +0900

    Create bcmc_forward.h

---
+++ main/bcmc_forward.h
@@ -0,0 +1,17 @@
+#ifndef DJMESH_BCMC_FORWARD_H
+#define DJMESH_BCMC_FORWARD_H
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void start_bcmc_forward();
+    void stop_bcmc_forward();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
