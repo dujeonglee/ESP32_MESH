cc9080a7a320557308277c27b439b174b076a800
Dujeong Lee <dujeong.lee82@gmail.com>
2018-08-11 23:46:56 +0900

    Update flash.h

--- main/flash.h
+++ main/flash.h
@@ -10,10 +10,14 @@ extern "C"
     void getSSID(char* ssid, uint32_t* length);
     void getPASSWORD(char* passwd, uint32_t* length);
     void getMode(uint8_t* mode);
+    void getConnectionRetry(uint8_t* retry);
+    void getMagic(uint8_t* magic);
 
     void setSSID(const char* ssid, uint32_t length);
     void setPASSWORD(const char* passwd, uint32_t length);
     void setMode(const uint8_t mode);
+    void setConnectionRetry(const uint8_t retry);
+    void setMagic(const uint8_t magic);
 #ifdef __cplusplus
 }
 #endif
