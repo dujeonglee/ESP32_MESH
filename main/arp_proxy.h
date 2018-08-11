d3042870900587872439a1ff3f57a8a9eedda58a
Dujeong Lee <dujeong.lee82@gmail.com>
2018-08-11 23:43:48 +0900

    Update arp_proxy.h

--- main/arp_proxy.h
+++ main/arp_proxy.h
@@ -1,6 +1,10 @@
 #ifndef DJMESH_ARP_PROXY_H
 #define DJMESH_ARP_PROXY_H
 #include <stdint.h>
+#include "netif/ethernet.h"
+#include "netif/raw_ethernet.h"
+#include "netif/etharp.h"
+#include "esp_wifi.h"
 
 #ifdef __cplusplus
 extern "C"
@@ -9,6 +13,11 @@ extern "C"
 
     void start_arp_proxy();
     void stop_arp_proxy();
+    err_t etharp_raw(struct netif *netif, const struct eth_addr *ethsrc_addr,
+                 const struct eth_addr *ethdst_addr,
+                 const struct eth_addr *hwsrc_addr, const ip4_addr_t *ipsrc_addr,
+                 const struct eth_addr *hwdst_addr, const ip4_addr_t *ipdst_addr,
+                 const u16_t opcode);
 
 #ifdef __cplusplus
 }
